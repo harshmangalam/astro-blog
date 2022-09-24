d04bfaa9bdb7e719320cd015e854c285103f98bd
harshmangalam <harshdev8218@gmail.com>
2022-09-24 13:55:01 +0530

    added dark mode

--- src/components/ThemeToggler.jsx
+++ src/components/ThemeToggler.jsx
@@ -1,6 +1,41 @@
+import { createEffect, Show } from "solid-js";
+import { createSignal } from "solid-js";
+import SunIcon from "./icons/SunIcon";
+import MoonIcon from "./icons/MoonIcon";
 export default function ThemeToggler() {
-  const handleToggle = () => {
+  const [darkMode, setDarkMode] = createSignal(false);
+
+  const applyDarkMode = () => {
+    localStorage.theme = "dark";
     document.documentElement.classList.add("dark");
+    setDarkMode(true);
   };
-  return <button onClick={handleToggle}>Toggle</button>;
+
+  const applyLightMode = () => {
+    localStorage.theme = "light";
+    document.documentElement.classList.remove("dark");
+    setDarkMode(false);
+  };
+
+  createEffect(() => {
+    const hasThemeKey = "theme" in localStorage;
+    const darkInStorage = localStorage.theme === "dark";
+    const darkColorScheme = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+
+    darkInStorage || (!hasThemeKey && darkColorScheme)
+      ? applyDarkMode()
+      : applyLightMode();
+  });
+  const handleToggle = () => {
+    darkMode() ? applyLightMode() : applyDarkMode();
+  };
+  return (
+    <button onClick={handleToggle}>
+      <Show when={darkMode()} fallback={<MoonIcon />}>
+        <SunIcon />
+      </Show>
+    </button>
+  );
 }
